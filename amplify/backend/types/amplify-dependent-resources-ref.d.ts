6c69b22bc48d4358268770b34ed68de9cadaf2f9
Ng Han Inn <nghaninn@gmail.com>
2022-10-24 00:51:35 +0800

    Revert spelling error for changePythonRestAPI

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,6 +5,12 @@ export type AmplifyDependentResourcesAttributes = {
             "Arn": "string",
             "Region": "string",
             "LambdaExecutionRole": "string"
+        },
+        "changePythonRestAPI": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
         }
     },
     "api": {
